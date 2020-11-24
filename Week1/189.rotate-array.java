83d2aab3c5c7b8993e1b05d443a58a4c197b8c5b
richieyan <richieyan@gmail.com>
2020-11-24 08:43:18 +0800

    feat: week1 rotate-array2

--- Week1/189.rotate-array.java
+++ Week1/189.rotate-array.java
@@ -2,6 +2,21 @@
  * @lc app=leetcode id=189 lang=java
  *
  * [189] Rotate Array
+
+// 暴力法
+     private void moveRight(int[] nums){
+        int v = nums[nums.length - 1];
+        for(int j=nums.length-1;j>0;j--){
+            nums[j] = nums[j-1];
+        }
+        nums[0] = v;
+    }
+    public void rotate(int[] nums, int k) {
+        // 暴力法
+        for(int i=0;i<k;i++){
+            moveRight(nums);
+        }
+    }
  */
 
 // @lc code=start
