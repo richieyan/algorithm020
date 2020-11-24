aa3eba43444f226ac9ca3d7e85cbef8d9c8bd5d5
richieyan <richieyan@gmail.com>
2020-11-24 08:51:49 +0800

    feat: week1 rotate-array3

--- Week1/189.rotate-array.java
+++ Week1/189.rotate-array.java
@@ -4,20 +4,42 @@
  * [189] Rotate Array
 
 // 暴力法
-     private void moveRight(int[] nums){
-        int v = nums[nums.length - 1];
-        for(int j=nums.length-1;j>0;j--){
-            nums[j] = nums[j-1];
-        }
-        nums[0] = v;
+private void moveRight(int[] nums){
+    int v = nums[nums.length - 1];
+    for(int j=nums.length-1;j>0;j--){
+        nums[j] = nums[j-1];
     }
-    public void rotate(int[] nums, int k) {
-        // 暴力法
-        for(int i=0;i<k;i++){
-            moveRight(nums);
-        }
+    nums[0] = v;
+}
+public void rotate(int[] nums, int k) {
+    // 暴力法
+    for(int i=0;i<k;i++){
+        moveRight(nums);
     }
- */
+}
+
+
+// 旋转法
+private static void swap(int[] nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+// TODO reverse with start&end
+private static void reverse(int[] nums, int start, int end) {
+    int len = nums.length;
+    if (len <= 1) {
+        return;
+    }
+    int times = len / 2;
+    for (int i = 0; i < times; i++) {
+        swap(nums, i, len - i - 1);
+    }
+} 
+
+
+*/
 
 // @lc code=start
 class Solution {
